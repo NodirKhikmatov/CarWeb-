01dfbe9771129197f39300e88dbf2c0682194ac2
Nodir <nodirhikmatov6gmail.com>
2025-03-04 06:37:05 +0900

    feat: integrate middleWare session

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,15 @@ import routerAdmin from "./routerAdmin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
+
 /** 1-entrance */
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
@@ -12,7 +21,19 @@ app.use(express.urlencoded({ extended: true })); //htmlni handle qilish traditio
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
-/** sessions */
+/** 2-sessions */
+
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 /** 3-views */
 app.set("views", path.join(__dirname, "views"));
