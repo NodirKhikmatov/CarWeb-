0833f7c9215937ff834896fec6e5dc8bb6095829
Nodir <nodirhikmatov6gmail.com>
2025-03-04 07:02:40 +0900

    feat: create session on login and signup process

--- src/controller/Car.controller.ts
+++ src/controller/Car.controller.ts
@@ -3,6 +3,8 @@ import { T } from "../libs/types/common";
 import { Request, Response } from "express";
 import MemberService from "../models/Member.service";
 import { MemberType } from "../libs/enums/member.enum";
+import session from "express-session";
+import { AdminRequest } from "../libs/types/member";
 
 const carController: T = {};
 
@@ -35,7 +37,7 @@ carController.signUp = (req: Request, res: Response) => {
   }
 };
 
-carController.processLogin = async (req: Request, res: Response) => {
+carController.processLogin = async (req: AdminRequest, res: Response) => {
   try {
     console.log("processLogin");
     console.log("req", req.body);
@@ -43,23 +45,30 @@ carController.processLogin = async (req: Request, res: Response) => {
     const input: LoginInput = req.body,
       result = await memberService.processLogin(input);
 
-    res.send(result);
+    req.session.member = result;
+    req.session.save(function () {
+      res.send(result);
+    });
   } catch (err) {
     console.log("Error: ", err);
     res.send(err);
   }
 };
 
-carController.processSignup = async (req: Request, res: Response) => {
+carController.processSignup = async (req: AdminRequest, res: Response) => {
   try {
     console.log("processSignup");
     console.log("body", req.body);
 
     const newMember: MemberInput = req.body;
     newMember.memberType = MemberType.CAR;
-
     const result = await memberService.processSignup(newMember);
-    res.send(result);
+
+    //todo session authentication
+    req.session.member = result;
+    req.session.save(function () {
+      res.send(result);
+    });
   } catch (err) {
     console.log("Error: ", err);
     res.send(err);
